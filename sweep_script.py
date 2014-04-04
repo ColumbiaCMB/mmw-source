687b07c3226517045aec4f4f224472e9a8f1de0a
ckjellstrand <bkjell@gmail.com>
2014-04-04 18:00:39 -0400

    Added automatic filesaving to sweep_script using np.savez.

--- sweep_script.py
+++ sweep_script.py
@@ -2,14 +2,15 @@ import lockin_controller
 import hittite_controller
 import numpy as np
 import time
+import os
 
 l=lockin_controller.lockinController(serial_port='COM6')
 # For running this script on the windows laptop by the source.
 h=hittite_controller.hittiteController()
 
 start=10e9
-stop=14e9
-step=.05e9
+stop=14.05e9
+step=0.05e9
 
 freq_list=[]
 x_list=[]
@@ -22,7 +23,7 @@ time.sleep(0.1)
 for freq in np.arange(start,stop,step):
     freq_list.append(freq)
     h.set_freq(float(freq))
-    time.sleep(5)
+    time.sleep(20)
     data_string=l.get_data()
     data_string=data_string.strip('\r')
     data_list=data_string.split(',')
@@ -31,6 +32,12 @@ for freq in np.arange(start,stop,step):
     r_list.append(float(data_list[2]))
     theta_list.append(float(data_list[3]))
 h.off()
+
+fn=time.strftime('%Y-%m-%d_%H-%M-%S')
+base_dir='C:\Users\lab computer\Documents\data\mmwave_source_sweeps'
+fn = os.path.join(base_dir,fn)
+fn+='.npz'
+np.savez(fn, freq=freq_list, x=x_list, y=y_list, r=r_list, theta=theta_list)
 
 print freq_list
 print x_list
