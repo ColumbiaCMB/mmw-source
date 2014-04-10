592b782ddfe270fcf97a3a0de635f222794a93e2
ckjellstrand <cbk2120@columbia.edu>
2014-04-10 11:23:07 -0400

    Wrote a short script to automatically import and plot data. Including the different resolution data in the data_for_processing directory.

---
+++ plot_sweep_data.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as pyplot
+import interpolater
+
+data_files=  [
+            'high_res_0_turns.npz','high_res_3_turns_minnie.npz','high_res_7_turns_minnie.npz',
+            'high_res_7_turns_both.npz','high_res_8_turns_both.npz','high_res_11_turns_both.npz'
+            ]
+
+interp=interpolater.interpolater()
+pow_list=range(len(data_files))
+freq_list=range(len(data_files))
+mult_list=range(len(data_files))
+prefix='./data_for_processing/'
+
+for i in range(len(data_files)):
+    data=np.load(prefix+data_files[i])
+    pow_list[i],freq_list[i],mult_list[i]=interp.convert(data['freq'],data['x'])
+    
+pyplot.subplot(1,1,1)
+for i in range(len(data_files)):
+    pyplot.plot(freq_list[i],pow_list[i])
+pyplot.yscale('log')
+pyplot.show()
