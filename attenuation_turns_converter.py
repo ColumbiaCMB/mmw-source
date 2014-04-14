fdb7c8e2b9cb9d14d27d7f2affb41c2645173e24
ckjellstrand <cbk2120@columbia.edu>
2014-04-14 15:28:54 -0400

    Expanded attenuation_turns_converter and started writing a notebook to display data analysis with it.

--- attenuation_turns_converter.py
+++ attenuation_turns_converter.py
@@ -8,13 +8,10 @@ class attenuationConverter():
         datafile='2014-04-11_broadband-attenuator-response.npz'
         self.data=np.load(prefix+datafile)
         self.output_to_db()
-        #self.interpolate_db()
         
     def output_to_db(self):
-        #self.db_list=range(len(self.data['x']))
-        self.db_list=range(46)
-        #for i in range(len(self.data['x'])):
-        for i in range(46):
+        self.db_list=range(len(self.data['x']))
+        for i in range(len(self.db_list)):
             self.db_list[i]=self.data['x'][i]/self.data['x'][0]
             self.db_list[i]=np.log10(abs(self.db_list[i]))
             # Abs is so negative values don't ruin the interpolation.
@@ -23,18 +20,19 @@ class attenuationConverter():
             
     def interpolate_db(self):
         #self.interpolating_function=interpolate.splrep(self.data['mickey_turns'][:46],self.db_list,s=0)
-        plt.plot(self.db_list,self.data['mickey_turns'][:46],'--o')
+        plt.plot(self.db_list[:46],self.data['mickey_turns'][:46],'--o')
         self.interpolating_function=interpolate.splrep(self.db_list,self.data['mickey_turns'][:46],s=0)
         # Why doesn't this work? After cutting off the noise, the function doesn't have multiple y values for a single x value.
         
 
     def give_turns(self, attenuation):
-        atten_reduced=np.array(self.db_list)-attenuation
+        # Only use the first 46 points because after that we hit the noise floor (and interpolating for y fails since there are multiple x-values.)
+        atten_reduced=np.array(self.db_list[:46])-attenuation
         freduced=interpolate.UnivariateSpline(self.data['mickey_turns'][:46],atten_reduced,s=0)
         return freduced.roots()
         
-    def give_watts(self):
-        volt_list=self.data['x']
+    def give_watts(self, data_list):
+        volt_list=data_list
         first_harmonic_factor=np.pi/2.0
         rms_factor=np.sqrt(2)
         for i in range(len(volt_list)):
@@ -48,8 +46,8 @@ class attenuationConverter():
         # Power list is now in watts.
         return pow_list
         
-    def give_dbm(self):
-        pow_list=self.give_watts()
+    def give_dbm(self, data_list):
+        pow_list=self.give_watts(data_list)
         for i in range(len(pow_list)):
             pow_list[i]=np.log10(abs(pow_list[i]))*10.0+30
             # Abs needed because a) power can't be negative and b) because negative voltages are due to the noise floor.
